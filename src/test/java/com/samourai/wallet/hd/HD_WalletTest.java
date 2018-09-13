6cf9be61a0306cdb1fcdf5142e49e853946219df
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-09-13 14:58:34 +0200

    update test for recent change in HD_Wallet.computeRootKey()

--- src/test/java/com/samourai/wallet/hd/HD_WalletTest.java
+++ src/test/java/com/samourai/wallet/hd/HD_WalletTest.java
@@ -26,29 +26,29 @@ public class HD_WalletTest {
         HD_Wallet hdWallet1Copy = new HD_Wallet(44, hdWallet1, 1);
 
         // verify
-        Assertions.assertArrayEquals(new String[]{"tpubDCLffiuyQ6v4AhNLp1FJiJYcuJv1pdA14dunQdhfpUWpYmkAZuhEnNNubo3fxMb3iWKb3jpkpWspH5YBRSzwfDuiCrsJR4UEXnobLJ3mKJa"}, hdWallet1.getXPUBs());
-        Assertions.assertArrayEquals(new String[]{"tpubDCLffiuyQ6v4AhNLp1FJiJYcuJv1pdA14dunQdhfpUWpYmkAZuhEnNNubo3fxMb3iWKb3jpkpWspH5YBRSzwfDuiCrsJR4UEXnobLJ3mKJa"}, hdWallet1Copy.getXPUBs());
-        Assertions.assertArrayEquals(new String[]{"tpubDCWCxiYrpwtjMzdftG9pdcMEj4mRqwpRvVGGzduu4eT9ZjLRPsHTbTsejNz1WufxcY55PMVabkAaYuP76Qau576XZu8qAP8TpFT2Skc1k2X"}, hdWallet2.getXPUBs());
-        Assertions.assertArrayEquals(new String[]{"tpubDDr2YdixLfDwauFXHYoM92pKjh8bL1RBrF6XqfpsibUoUCj5ouj44CV3ufBk79rhv2L8tXWyH9m4DshUTdcZ1iRtVhXTBcN9ksoJk3jDksV"}, hdWallet3.getXPUBs());
-        Assertions.assertArrayEquals(new String[]{"tpubDDn8QvxpH2z7gb4acz6iwqLBHfbSWkFnLi3eZ1gLyGSYbyD2NaFq8cW6SNkQF22uf2Den9ZqKXxfUynNo1ieMHEktR9h9sbbh4ijbdXCLiU"}, hdWallet4.getXPUBs());
+        Assertions.assertArrayEquals(new String[]{"tpubDC8qnx32oEZVipETHVHJhybpkTrgadwLGQZqd2nz9VPVu63gH4R6BHKB1UB4DYwXNu37Dtw1JmADsZQ75upg4Dy8aBCBCR28mDuC86DKueS"}, hdWallet1.getXPUBs());
+        Assertions.assertArrayEquals(new String[]{"tpubDC8qnx32oEZVipETHVHJhybpkTrgadwLGQZqd2nz9VPVu63gH4R6BHKB1UB4DYwXNu37Dtw1JmADsZQ75upg4Dy8aBCBCR28mDuC86DKueS"}, hdWallet1Copy.getXPUBs());
+        Assertions.assertArrayEquals(new String[]{"tpubDCjWUQzx4WG2igBV4MtRbYg8ZkgsSs3LBs4Y1rBmF8MbFeGCcvFFPmqLgbpr1bNy37fnwsfm9SCkgcUYBw4hgVFzLsfwxoCqWkBiWhbU9Ry"}, hdWallet2.getXPUBs());
+        Assertions.assertArrayEquals(new String[]{"tpubDCCzbVg6SzMF9fYUTuRbqPdcUFu2erTn2iHHgTp1jxu7Ve9zxLSvPVdfxR6nRT6GE5ShqM35eTfKXMhncSHCkrZzTEdgbZ8tFs63ix6S1fC"}, hdWallet3.getXPUBs());
+        Assertions.assertArrayEquals(new String[]{"tpubDDK5Q8DUwp9TvNZjf6QBAUuxk2QAgcmpVVpxSHVYc4fDBuCpwuyQnzDDAtimCBLDrQXrX3R8KKoyjWjVABZtfE41FWeYSVKndfKCvmXrhD6"}, hdWallet4.getXPUBs());
 
-        Assertions.assertEquals("mrZ4DHwdzK4pAuKnoVL4vaeHeYe8e12hn2", hdWallet1.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
-        Assertions.assertEquals("mrZ4DHwdzK4pAuKnoVL4vaeHeYe8e12hn2", hdWallet1Copy.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
-        Assertions.assertEquals("mxQEdFhaXGWf3a6G4M5Ti11uwUXBEQW2FZ", hdWallet2.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
-        Assertions.assertEquals("mwTMnoSzutrExZWd9zJRtehXBwbEerpdtM", hdWallet3.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
-        Assertions.assertEquals("moupnWvhQxzFZWRQSPoMaeMY6RPGsMCnQ6", hdWallet4.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("n3VABKp2wDB3mLHw6xH6SgVMWzFGU9u169", hdWallet1.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("n3VABKp2wDB3mLHw6xH6SgVMWzFGU9u169", hdWallet1Copy.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("myrDKvdCUNAMEoxWT4r3116i21R93s5vUV", hdWallet2.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("mpZYLcbacAdehXp3max1h2Lubk4GRnnpLj", hdWallet3.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("mhAaH3UGm6NYeHHvHx3KRGGCddwYdBj3VH", hdWallet4.getAccount(0).getChain(0).getAddressAt(0).getAddressString());
 
-        Assertions.assertEquals("mpL7ecpEyzUSr1y2x8HgxzN4ZRaVS7mFKQ", hdWallet1.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
-        Assertions.assertEquals("mpL7ecpEyzUSr1y2x8HgxzN4ZRaVS7mFKQ", hdWallet1Copy.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
-        Assertions.assertEquals("miStZQEnDz895mxQD9yCUXh7PqEwza8Ezj", hdWallet2.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
-        Assertions.assertEquals("mszNy5PjqR6Fn8bwj89Ns1BkqjENhBWsyq", hdWallet3.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
-        Assertions.assertEquals("n3VnQGNEy6XGzJmK9d4tfSJWFDbc62uRf5", hdWallet4.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("mt6uJ69jhbFGzAgrb6RnEqhwrd7tTKjxF7", hdWallet1.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("mt6uJ69jhbFGzAgrb6RnEqhwrd7tTKjxF7", hdWallet1Copy.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("mnTHShP3iqCBYnxDnnBswpaHW3gMnVQyYq", hdWallet2.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("mq8cBiuaRRPUyue22TMbWerhruXrJmfGZY", hdWallet3.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
+        Assertions.assertEquals("mnEntMG6XAqwvH9C1UbHiKgwdWFKAEgHtu", hdWallet4.getAccount(0).getChain(1).getAddressAt(0).getAddressString());
 
-        Assertions.assertEquals("mg6xV9QGhkxqrTFeBpLKnJ3ERbW7qPFXwd", hdWallet1.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
-        Assertions.assertEquals("mg6xV9QGhkxqrTFeBpLKnJ3ERbW7qPFXwd", hdWallet1Copy.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
-        Assertions.assertEquals("mmftBoRdUdP79S9dBh2gYzyNzpqHsT12nR", hdWallet2.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
-        Assertions.assertEquals("mttGD9kAtcA4a4Uu2WC8AZf1tdM8VdqvC1", hdWallet3.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
-        Assertions.assertEquals("mmXjbaYTVSp9bUTLdkQF7QrYsRAFbqUMxc", hdWallet4.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
+        Assertions.assertEquals("muimRQFJKMJM1pTminJxiD5HrPgSu257tX", hdWallet1.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
+        Assertions.assertEquals("muimRQFJKMJM1pTminJxiD5HrPgSu257tX", hdWallet1Copy.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
+        Assertions.assertEquals("n2HYGk5jk4YoRQrBeMNkE5RKHegRgVyU9M", hdWallet2.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
+        Assertions.assertEquals("mpZbQ8syt9MNRh9duiwVaEsVYwQE5E6r5p", hdWallet3.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
+        Assertions.assertEquals("mmiiiy2gW4KFdKXQJC93p3jpjS7FXpi8Lq", hdWallet4.getAccount(0).getChain(1).getAddressAt(1).getAddressString());
 
     }
 
