fc4c3704554acb6362a427ab3e93fb8b8824d95c
zeroleak <e17ba2c64b67f25a1cddb9bd80f4c53a2df7e9b3>
2018-11-04 21:35:58 +0100

    add TxUtil.signInputSegwit() + TxUtil.findInputIndex()

---
+++ java/com/samourai/wallet/util/TxUtil.java
@@ -0,0 +1,50 @@
+package com.samourai.wallet.util;
+
+import com.samourai.wallet.segwit.SegwitAddress;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.core.TransactionInput;
+import org.bitcoinj.core.TransactionOutPoint;
+import org.bitcoinj.core.TransactionWitness;
+import org.bitcoinj.crypto.TransactionSignature;
+import org.bitcoinj.script.Script;
+
+public class TxUtil {
+  private static TxUtil instance = null;
+
+  public static TxUtil getInstance() {
+    if(instance == null) {
+      instance = new TxUtil();
+    }
+    return instance;
+  }
+
+  public void signInputSegwit(Transaction tx, int inputIdx, ECKey ecKey, long spendAmount, NetworkParameters params) {
+    final SegwitAddress segwitAddress = new SegwitAddress(ecKey, params);
+    final Script redeemScript = segwitAddress.segWitRedeemScript();
+    final Script scriptCode = redeemScript.scriptCode();
+
+    TransactionSignature sig =
+        tx.calculateWitnessSignature(
+            inputIdx, ecKey, scriptCode, Coin.valueOf(spendAmount), Transaction.SigHash.ALL, false);
+    final TransactionWitness witness = new TransactionWitness(2);
+    witness.setPush(0, sig.encodeToBitcoin());
+    witness.setPush(1, ecKey.getPubKey());
+    tx.setWitness(inputIdx, witness);
+  }
+
+  public Integer findInputIndex(Transaction tx, String txoHash, long txoIndex) {
+    for (int i = 0; i < tx.getInputs().size(); i++) {
+      TransactionInput input = tx.getInput(i);
+      TransactionOutPoint outPoint = input.getOutpoint();
+      if (outPoint.getHash().toString().equals(txoHash) && outPoint.getIndex() == txoIndex) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+
+}
