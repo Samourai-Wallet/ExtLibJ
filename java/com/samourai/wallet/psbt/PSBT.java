e812edd559387fe430220e79f2e3f520ba6eb603
TDevD <dev@samouraiwallet.com>
2018-11-18 14:20:43 +0100

    add PSBT magic value

---
+++ java/com/samourai/wallet/psbt/PSBT.java
@@ -0,0 +1,12 @@
+package com.samourai.wallet.psbt;
+
+//
+// Partially Signed Bitcoin Transaction Format
+//
+public class PSBT {
+
+    public static final String PSBT_MAGIC = "70736274";
+
+    private PSBT() { ; }
+
+}
