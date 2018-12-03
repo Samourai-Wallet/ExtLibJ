0026e4340209b0946112f98ce61ac2181ed6114b
TDevD <dev@samouraiwallet.com>
2018-12-03 21:13:29 +0100

    don't use MethodHandles.lookup(), class not found on Android 5, 6, 7

--- java/com/samourai/wallet/bip47/rpc/PaymentCode.java
+++ java/com/samourai/wallet/bip47/rpc/PaymentCode.java
@@ -2,7 +2,6 @@ package com.samourai.wallet.bip47.rpc;
 
 import com.samourai.wallet.bip47.rpc.secretPoint.ISecretPoint;
 import com.samourai.wallet.hd.HD_Address;
-import java.lang.invoke.MethodHandles;
 import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
@@ -22,7 +21,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PaymentCode {
-    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private static final int PUBLIC_KEY_Y_OFFSET = 2;
     private static final int PUBLIC_KEY_X_OFFSET = 3;
@@ -239,7 +237,6 @@ public class PaymentCode {
     private static byte[] xor(byte[] a, byte[] b) {
 
         if(a.length != b.length)    {
-            log.error("Invalid length for xor: " + a.length + " vs " + b.length);
             return null;
         }
 
